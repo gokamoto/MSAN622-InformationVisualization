ad883c3e4be03c0d27be5ba7657a99e8fb156811
Griffin Okamoto <griffinokamoto@gmail.com>
2015-04-24 18:54:51 -0700

    final versions

--- HW3/dataToJSON.R
+++ HW3/dataToJSON.R
@@ -1,7 +1,12 @@
 library(jsonlite)
+library(zoo)
 
 setwd("~/Google Drive/USF/Spring2/InformationVisualization/MSAN622-InformationVisualization/HW3")
-data = as.data.frame(Seatbelts)
+
+month = seq(from=as.Date("1969-01-01"), to=as.Date("1984-12-01"), by="month")
+
+data = data.frame(Seatbelts)
+data$month = month
 
 json <- toJSON(
   data,
